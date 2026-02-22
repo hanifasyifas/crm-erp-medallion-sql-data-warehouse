d0ed61add80fdcf13d10c563d0b9db8dfaef69f3
hanifasyifas <hanifasyifas@gmail.com>
2026-02-22 14:18:47 +0700

    Create quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,408 @@
+/* 
+GOLD LAYER 
+
+*/
+
+-- ========================================
+-- Create Dimension: Customers
+-- ========================================
+
+select *
+from silver.crm_cust_info
+
+select
+	ci.cst_id,
+	ci.cst_key,
+	ci.cst_firstname,
+	ci.cst_lastname,
+	ci.cst_marital_status,
+	ci.cst_gndr,
+	ci.cst_create_date,
+	ca.bdate,
+	ca.gen,
+	la.cntry
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+
+-- After joining table, Check any duplicates were introduced by the join logic
+select cst_id, COUNT(*) from
+(select
+	ci.cst_id,
+	ci.cst_key,
+	ci.cst_firstname,
+	ci.cst_lastname,
+	ci.cst_marital_status,
+	ci.cst_gndr,
+	ci.cst_create_date,
+	ca.bdate,
+	ca.gen,
+	la.cntry
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+)t group by cst_id
+having count(*) > 1;
+-- No any duplicates
+
+-- Data integration for gender
+select distinct
+	ci.cst_gndr,
+	ca.gen,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master for gender info
+		else COALESCE(ca.gen, 'n/a') 
+	end as new_gen
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+order by 1,2
+
+-- Take it to the original query
+select
+	ci.cst_id,
+	ci.cst_key,
+	ci.cst_firstname,
+	ci.cst_lastname,
+	ci.cst_marital_status,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master for gender info
+		else COALESCE(ca.gen, 'n/a') 
+	end as new_gen,	
+	ci.cst_create_date,
+	ca.bdate,
+	la.cntry
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+
+-- Rename columns to friendly, meaningful names
+select
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as first_name,
+	ci.cst_lastname as last_name,
+	ci.cst_marital_status as marital_status,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master for gender info
+		else COALESCE(ca.gen, 'n/a') 
+	end as gender,	
+	ci.cst_create_date as create_date,
+	ca.bdate as birthdate,
+	la.cntry as country
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+
+-- Sort the columns into logical groups to improve readability
+select
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as first_name,
+	ci.cst_lastname as last_name,
+	la.cntry as country,
+	ci.cst_marital_status as marital_status,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master for gender info
+		else COALESCE(ca.gen, 'n/a') 
+	end as gender,
+	ca.bdate as birthdate,
+	ci.cst_create_date as create_date
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+
+-- Decides: Dimension or Fact Table? -> Dimension
+
+-- Create Surrogate Key
+select
+row_number() over (order by cst_id) as customer_key,
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as first_name,
+	ci.cst_lastname as last_name,
+	la.cntry as country,
+	ci.cst_marital_status as marital_status,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master for gender info
+		else COALESCE(ca.gen, 'n/a') 
+	end as gender,
+	ca.bdate as birthdate,
+	ci.cst_create_date as create_date
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+
+-- Create object view
+create view gold.dim_customers as 
+select
+row_number() over (order by cst_id) as customer_key,
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as first_name,
+	ci.cst_lastname as last_name,
+	la.cntry as country,
+	ci.cst_marital_status as marital_status,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master for gender info
+		else COALESCE(ca.gen, 'n/a') 
+	end as gender,
+	ca.bdate as birthdate,
+	ci.cst_create_date as create_date
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+
+-- Check the 0bject quality
+select * 
+from gold.dim_customers
+
+select distinct gender
+from gold.dim_customers
+
+-- ========================================
+-- Create Dimension: Products
+-- ========================================
+
+select
+pn.prd_id,
+pn.cat_id,
+pn.prd_key,
+pn.prd_nm,
+pn.prd_cost,
+pn.prd_line,
+pn.prd_start_dt,
+pn.prd_end_dt
+from silver.crm_prd_info pn
+
+-- Filter out historical data
+select
+pn.prd_id,
+pn.cat_id,
+pn.prd_key,
+pn.prd_nm,
+pn.prd_cost,
+pn.prd_line,
+pn.prd_start_dt
+from silver.crm_prd_info pn
+where prd_end_dt is null
+
+-- Joining with product categories from erp
+select
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null
+
+-- Check the quality: prd_key if there's any duplicates
+select prd_key, count(*) from(
+select
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null
+)t group by prd_key
+having count(*) > 1
+-- There's no duplicates
+
+-- Sort the columns into logical groups to improve readability
+select
+	pn.prd_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.cat_id,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null
+
+-- Rename columns to friendly, meaningful names
+select
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category,
+	pc.subcat as subcategory,
+	pc.maintenance,
+	pn.prd_cost as cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_date
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null
+
+-- Decides: Dimension or Fact Table? -> Dimension
+
+-- Create Surrogate Key
+select
+	row_number() over (order by pn.prd_start_dt, pn.prd_key) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category,
+	pc.subcat as subcategory,
+	pc.maintenance,
+	pn.prd_cost as cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_date
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null
+
+-- Create object view
+create view gold.dim_products as 
+select
+	row_number() over (order by pn.prd_start_dt, pn.prd_key) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category,
+	pc.subcat as subcategory,
+	pc.maintenance,
+	pn.prd_cost as cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_date
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null
+
+-- Check data quality
+select *
+from gold.dim_products
+
+-- ========================================
+-- Create Fact: Sales
+-- ========================================
+
+select 
+sd.sls_ord_num,
+sd.sls_prd_key,
+sd.sls_cust_id,
+sd.sls_order_dt,
+sd.sls_ship_dt,
+sd.sls_due_dt,
+sd.sls_sales,
+sd.sls_quantity,
+sd.sls_price
+from silver.crm_sales_details sd
+
+-- Dimension or Fact? -> Fact (Keys, Dates, Measures)
+
+-- Building Fact: Use the dimension's surrogate keys instead of IDs 
+-- to easily connect facts with dimensions
+
+-- Data Lookup: joining the tables in order only to get one information
+select 
+sd.sls_ord_num,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt,
+sd.sls_ship_dt,
+sd.sls_due_dt,
+sd.sls_sales,
+sd.sls_quantity,
+sd.sls_price
+from silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id
+
+-- Rename columns to friendly, meaningful names
+select 
+	sd.sls_ord_num as order_number,
+	pr.product_key,
+	cu.customer_key,
+	sd.sls_order_dt as order_date,
+	sd.sls_ship_dt as shipping_date,
+	sd.sls_due_dt as due_date,
+	sd.sls_sales as sales_amount,
+	sd.sls_quantity as quantity,
+	sd.sls_price as price
+from silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id
+
+-- Create object view
+create view gold.fact_sales as 
+select 
+	sd.sls_ord_num as order_number,
+	pr.product_key,
+	cu.customer_key,
+	sd.sls_order_dt as order_date,
+	sd.sls_ship_dt as shipping_date,
+	sd.sls_due_dt as due_date,
+	sd.sls_sales as sales_amount,
+	sd.sls_quantity as quantity,
+	sd.sls_price as price
+from silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id
+
+-- Check data quality
+select *
+from gold.fact_sales
+
+-- Check if all dimension tables can successfully join to the fact table
+select *
+from gold.fact_sales f
+left join gold.dim_customers c
+on c.customer_key = f.customer_key
+where c.customer_key is null
+-- No failed integration
+
+select *
+from gold.fact_sales f
+left join gold.dim_customers c
+on c.customer_key = f.customer_key
+left join gold.dim_products p
+on p.product_key = f.product_key
+where p.product_key is null
+-- No failed integration
+
